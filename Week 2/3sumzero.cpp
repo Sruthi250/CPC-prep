6175f57c2cdb3648bff72053ddcebc6898422d7a
Sruthi250 <63357472+Sruthi250@users.noreply.github.com>
2021-04-08 14:14:00 +0530

    Create 3sumzero.cpp

---
+++ Week 2/3sumzero.cpp
@@ -0,0 +1,47 @@
+//interviewbit
+vector<vector<int> > Solution::threeSum(vector<int> &A) {
+    
+        set<vector<int>> s;
+        vector<vector<int>> r;
+        vector<int> v;
+        int n = A.size();
+        sort(A.begin(),A.end());
+        
+        for(int i=0;i<n;i++){
+            int num = A[i];
+            int start = i+1;
+            int end = n-1;
+            
+            while(start < end){
+                int a = A[start];
+                int b = A[end];
+                
+                if(a+b == abs(num)){
+                    v.clear();
+                    v.push_back(num);
+                    v.push_back(a);
+                    v.push_back(b);
+                    
+                    s.insert(v);
+                    start++;
+                    end--;
+                }
+                
+                else if(a+ b > abs(num)){
+                    end--;
+                }
+                else{
+                    start++;
+                }
+            }
+        }
+        
+        set<vector<int>>:: iterator it;
+        
+        for(it = s.begin(); it!= s.end(); it++){
+            r.push_back(*it);
+        }
+        
+        return r;
+    }
+
